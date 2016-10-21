f265cc2fd950433348a9259a409cddf0950f4db4
camargo.ribeiro <linkcamargo@gmail.com>
2016-10-20 22:15:50 -0200

    funções de ler, editar e excluir

--- app/controllers/clientsController.js
+++ app/controllers/clientsController.js
@@ -1,8 +1,25 @@
-app.controller('clientsController', function($scope, sharedValues) {
+app.controller('clientsController', function($scope, $location, sharedValues) {
   $scope.module = "Clientes";
+  $scope.client = sharedValues.getObject();
 
   $.getJSON("mocks/clients/clients.json", function(json) {
     $scope.clientsList = json;
   });
+
+  $scope.edit = function(val){
+  	$scope.client = val;
+  	sharedValues.setObject($scope.client);
+  	$location.path("entries/clients");
+  };
+
+  $scope.read = function(val){
+  	$scope.client = val;
+  	sharedValues.setObject($scope.client);
+  	$location.path("entries/clients");
+  };
+
+  $scope.remove = function(val) {
+    $scope.clientsList.splice($scope.clientsList.indexOf(val),1);
+  };
 
 });
